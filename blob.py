412928dc4419c73e338c25d8785b48b777f7b838
lmenale <luigi.menale@technica-engineering.de>
2018-06-18 11:43:38 +0200

    Added the source files

---
+++ blob.py
@@ -0,0 +1,37 @@
+import random
+
+class Blob:
+    
+    def __init__(self, color, x_boundary, y_boundary, size_range=(4, 8), move_range=(-1, 2)):
+        self.x_boundary = x_boundary
+        self.y_boundary = y_boundary
+        self.size = random.randrange(size_range[0], size_range[1])
+        self.color = color
+        self.move_range = move_range
+        self.x = random.randrange(0, self.x_boundary)
+        self.y = random.randrange(0, self.y_boundary)
+    
+    def __repr__(self):
+        return 'Blob({}, {}, ({}, {}))'.format(self.color,
+                                                self.size,
+                                                self.x,
+                                                self.y)
+    
+    def __str__(self):
+        return 'Blob color:{}, size:{}, location:({}, {}))'.format(self.color,
+                                                self.size,
+                                                self.x,
+                                                self.y)
+
+    def move(self):
+        self.move_x = random.randrange(self.move_range[0], self.move_range[1])
+        self.move_y = random.randrange(self.move_range[0], self.move_range[1])
+        self.x += self.move_x
+        self.y += self.move_y
+
+    def check_bounds(self):
+        if self.x < 0: self.x = 0
+        elif self.x > self.x_boundary: self.x = self.x_boundary
+
+        if self.y < 0: self.y = 0
+        elif self.y > self.y_boundary: self.y = self.y_boundary
